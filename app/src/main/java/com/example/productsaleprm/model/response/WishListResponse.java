4e72cf737d41afd8b2938e00bfeacf9ecfe9a9a0
Lê Quang Ánh <superanhziggy@gmail.com>
2025-07-11 15:55:26 +0700

    cart

---
+++ app/src/main/java/com/example/productsaleprm/model/response/WishListResponse.java
@@ -0,0 +1,53 @@
+package com.example.productsaleprm.model.response;
+
+import com.example.productsaleprm.model.WishList;
+
+import java.util.List;
+
+public class WishListResponse {
+    private int totalItems;
+    private boolean isLast;
+    private int totalPages;
+    private int page;
+    private List<WishList> wishListItem;
+
+    public int getTotalItems() {
+        return totalItems;
+    }
+
+    public void setTotalItems(int totalItems) {
+        this.totalItems = totalItems;
+    }
+
+    public boolean isLast() {
+        return isLast;
+    }
+
+    public void setLast(boolean last) {
+        isLast = last;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public List<WishList> getWishListItem() {
+        return wishListItem;
+    }
+
+    public void setWishListItem(List<WishList> wishListItem) {
+        this.wishListItem = wishListItem;
+    }
+}

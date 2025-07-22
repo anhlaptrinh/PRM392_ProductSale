3a1d13eacf0bf04e0da811023478fb5058c6e47c
Long <longsmile2k@gmail.com>
2025-07-22 21:22:13 +0700

    category

--- app/src/main/java/com/example/productsaleprm/model/Category.java
+++ app/src/main/java/com/example/productsaleprm/model/Category.java
@@ -3,12 +3,15 @@ package com.example.productsaleprm.model;
 public class Category {
     private int categoryID;
     private String categoryName;
-
+    private String imageUrl;
     public int getCategoryID() {
         return categoryID;
     }
 
     public String getCategoryName() {
         return categoryName;
+    }
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
